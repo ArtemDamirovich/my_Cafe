9d3a4b50ecf5cf6712d7efbcf72091cbe59adf34
aurusxd <your_email@example.com>
2026-03-11 19:12:02 +0700

    cafe

---
+++ app/Models/Admin.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Wainter;
+
+class Admin extends Controller
+{
+    public function  assignRole(Request $request, Wainter $wainter)
+    {
+        
+    }
+}
