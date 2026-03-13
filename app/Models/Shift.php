3f0897ecc89d369f28b1dbe456e36c3ade211165
aurusxd <your_email@example.com>
2026-03-13 20:48:59 +0700

    cafe

--- app/Models/Shift.php
+++ app/Models/Shift.php
@@ -2,9 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Shift extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'date',
+        'shift_type',
+        'start_type',
+        'end_time'
+    ];
+
+    protected $casts = [
+        'date' => 'datetime:H:i',
+        'end_time' => 'datetime:H:i'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function volume_To_Date($query)
+    {
+        return $query->whereDate('date', now()->toDateString());
+    }
 }
--- app/Models/Table.php
+++ app/Models/Table.php
@@ -2,9 +2,42 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Table extends Model
 {
-    
+    use HasFactory;
+
+    protected $fillable = [
+        'table_id',
+        'status',
+        'seats'      
+    ];
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    public function current_Order()
+    {
+        return $this->hasMany(Order::class)->whereIn('status', ['new', 'cooking', 'ready']);
+    }
+
+    public function is_free()
+    {
+        return $this->status == 'free';
+    }
+
+    public function booked() // Забронированый столик
+    {
+        $this->status == 'booked';
+        $this->save();
+    }
+
+    public function busy() // Занятый столик
+    {
+        $this->status == 'free';
+        $this->save();
+    }
 }
